6200c9acc62f1458baf5d0588dca0757555d81b7
prodevghost <softwareengineer1014@gmail.com>
2021-07-28 00:45:31 +0800

    Change functions in ConverterV1
    - Change swap_exact_to_insure() function
    - Change swap_insure_to_exact() function
    - Change getAmountsIn() function

--- contracts/Converters/ConverterV1.sol
+++ contracts/Converters/ConverterV1.sol
@@ -12,10 +12,18 @@ import "../interfaces/ISwapRouter.sol";
 contract ConverterV1{
     using SafeMath for uint256;
 
-    ISwapRouter public UniswapV2 = ISwapRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); //rinkeby
+    ISwapRouter public UniswapV3 = ISwapRouter(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D); //rinkeby
     InsureToken public insure_token;
     IERC20 public WETH = IERC20(0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2); //rinkeby
     IERC20 public USDC = IERC20(0xeb8f08a975Ab53E34D8a0330E0D34de942C95926); //rinkyby
+
+    /// @dev The length of the bytes encoded address
+    uint256 private constant ADDR_SIZE = 20;
+    /// @dev The length of the bytes encoded fee
+    uint256 private constant FEE_SIZE = 3;
+
+    /// @dev The offset of a single token address and pool fee
+    uint256 private constant NEXT_OFFSET = ADDR_SIZE + FEE_SIZE;
 
     constructor(address _insure)public{
         insure_token = InsureToken(_insure);
@@ -27,15 +35,32 @@ contract ConverterV1{
     function swap_exact_to_insure(uint256 _amountIn, address _to)external returns(bool){
         //setup for swap
         require(USDC.transferFrom(msg.sender, address(this), _amountIn), 'transferFrom failed.');
-        require(USDC.approve(address(UniswapV2), _amountIn), 'approve failed.');
+        require(USDC.approve(address(UniswapV3), _amountIn), 'approve failed.');
 
-        address[] memory path = new address[](3);
-        path[0] = address(USDC);
-        path[1] = address(WETH);
-        path[2] = address(insure_token); //insure token
+        bytes tokenA = abi.encodePacked(address(USDC));
+        bytes tokenB = abi.encodePacked(address(insure_token));
+        uint24 feeAmount = _amountIn * 3 / 1000;
+        bytes fee = abi.encodePacked(feeAmount);
+
+        bytes memory path = new bytes(NEXT_OFFSET + ADDR_SIZE);
+        assembly {
+            mstore(add(path, ADDR_SIZE), tokenA);
+            mstore(add(path, NEXT_OFFSET), fee);
+            mstore(add(path, NEXT_OFFSET + ADDR_SIZE), tokenB);
+        }
+
+        bytes data = abi.encode(path, _to);
+
+        UniswapV3.uniswapV3SwapCallback(_amountIn, 0, data);
+
+
+        // address[] memory path = new address[](3);
+        // path[0] = address(USDC);
+        // path[1] = address(WETH);
+        // path[2] = address(insure_token); //insure token
 
         //swap
-        UniswapV2.swapExactTokensForTokens(_amountIn, 0, path, _to, block.timestamp.add(25));
+        // UniswapV2.swapExactTokensForTokens(_amountIn, 0, path, _to, block.timestamp.add(25));
 
         return true;
     }
@@ -45,29 +70,63 @@ contract ConverterV1{
     *@dev only be used in case of emergency_mint(). To know how many INSURE is required.
     */
     function getAmountsIn(uint256 _amountOut)external view returns(uint256){
-        address[] memory path = new address[](3);
-        path[0] = address(insure_token);
-        path[1] = address(WETH);
-        path[2] = address(USDC);
+        
+        bytes tokenA = abi.encodePacked(address(insure_token));
+        bytes tokenB = abi.encodePacked(address(USDC));
+        uint24 feeAmount = _amountOut * 3 / 1000;
+        bytes fee = abi.encodePacked(feeAmount);
 
-        uint256[] memory amountsIn = UniswapV2.getAmountsIn(_amountOut, path);
-        return amountsIn[0]; //required Insure Token
+        bytes memory path = new bytes(NEXT_OFFSET + ADDR_SIZE);
+        assembly {
+            mstore(add(path, ADDR_SIZE), tokenA);
+            mstore(add(path, NEXT_OFFSET), fee);
+            mstore(add(path, NEXT_OFFSET + ADDR_SIZE), tokenB);
+        }
+
+        address _to = 0;
+
+        bytes data = abi.encode(path, _to);
+
+        uint256 amountIn = UniswapV3.exactOutputInternal(_amountOut, msg.sender, 0, data);
+
+        return amountIn;
+
+        // uint256[] memory amountsIn = UniswapV2.getAmountsIn(_amountOut, path);
+        // return amountsIn[0]; //required Insure Token
     }
 
     /***
     *@dev only be used in case of emergency_mint(). Swap minted INSURE to USDC to make a payment.
     */
-    function swap_insure_to_exact(uint256 _amountInMax, uint256 _amountOut, address _to)external{
+    function swap_insure_to_exact(uint256 _amountInMax, uint256 _amountOut, address _to)external returns (bool){
         //setup for swap
         require(insure_token.transferFrom(msg.sender, address(this), _amountInMax), 'transferFrom failed.');
-        require(insure_token.approve(address(UniswapV2), _amountInMax), 'approve failed.');
+        require(insure_token.approve(address(UniswapV3), _amountInMax), 'approve failed.');
 
-        address[] memory path = new address[](3);
-        path[0] = address(insure_token); //insure token
-        path[1] = address(WETH);
-        path[2] = address(USDC);
+        bytes tokenA = abi.encodePacked(address(insure_token));
+        bytes tokenB = abi.encodePacked(address(USDC));
+        uint24 feeAmount = _amountOut * 3 / 1000;
+        bytes fee = abi.encodePacked(feeAmount);
+
+        bytes memory path = new bytes(NEXT_OFFSET + ADDR_SIZE);
+        assembly {
+            mstore(add(path, ADDR_SIZE), tokenA);
+            mstore(add(path, NEXT_OFFSET), fee);
+            mstore(add(path, NEXT_OFFSET + ADDR_SIZE), tokenB);
+        }
+
+        bytes data = abi.encode(path, _to);
+
+        UniswapV3.uniswapV3SwapCallback(_amountOut, _amountInMax, data);
+
+        // address[] memory path = new address[](3);
+        // path[0] = address(insure_token); //insure token
+        // path[1] = address(WETH);
+        // path[2] = address(USDC);
         
-        UniswapV2.swapTokensForExactTokens(_amountOut, _amountInMax, path, _to, block.timestamp.add(25));
+        // UniswapV2.swapTokensForExactTokens(_amountOut, _amountInMax, path, _to, block.timestamp.add(25));
+        
+        return true;
     }
 
 }
