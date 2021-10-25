693f28632247b5bad784c25f02094e25b2a85d1a
shun <shunoikawa1112@gmail.com>
2021-10-25 18:10:01 +0900

    add constant

--- contracts/FeeDistribution/ReportingDistributionV1.sol
+++ contracts/FeeDistribution/ReportingDistributionV1.sol
@@ -23,7 +23,7 @@ contract ReportingDistributionV1 is ReentrancyGuard{
     event CommitAdmin(address admin);
     event AcceptAdmin(address admin);
 
-    address public USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //mainnet USDC
+    address public constant USDC = 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48; //mainnet USDC
 
     address public insure_reporting; //SURERPT address
 
