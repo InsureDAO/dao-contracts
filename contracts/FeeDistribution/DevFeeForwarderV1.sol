1c2297f5295fcaace949cb9ba6398a5c9deffeeb
shun <shunoikawa1112@gmail.com>
2021-11-02 18:47:11 +0900

    Test FeeDistributors

--- contracts/FeeDistribution/DevFeeForwarderV1.sol
+++ contracts/FeeDistribution/DevFeeForwarderV1.sol
@@ -21,20 +21,24 @@ contract DevFeeForwarder is ReentrancyGuard{
     address public wallet; //gnosis multisig wallet
 
     constructor(address _wallet){
+        require(_wallet != address(0), "zero address");
         wallet = _wallet;
     }
 
     function distribute(address _token)external returns(bool){
         //anyone can call this contract
+
+        require(_token != address(0), "zero address");
+
         //collect _token.
         uint256 claimable = IERC20(_token).allowance(msg.sender, address(this));
         if(claimable > 0){
             IERC20(_token).safeTransferFrom(msg.sender, address(this), claimable);
         }
 
-        //transfer to insureDAO multisig wallet. *only this part can be excuted if msg.sender approve 0
-        uint256 amount = IERC20(_token).balanceOf(msg.sender);
-        IERC20(_token).safeTransferFrom(address(this), wallet, amount);
+        //transfer all token in this contract to the wallet.
+        uint256 amount = IERC20(_token).balanceOf(address(this));
+        IERC20(_token).safeTransfer(wallet, amount);
 
         return true;
     }
---
+++ test/unitary/FeeDistributors/DevFeeForwarderV1/test_dev_fee_forwarder_v1.js
@@ -0,0 +1,106 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+const { BigNumber } = require('ethers');
+
+describe('DevFeeForwarder', () => {
+    const name = "Fee Token";
+    const simbol = "FT";
+    const decimal = 18;
+    const rpt_decimal = 0;
+
+    const ZERO_ADDRESS = '0x0000000000000000000000000000000000000000';
+    const FEE = BigNumber.from("1000000")
+  
+    beforeEach(async () => {
+      [creator, alice, bob, chad, dad] = await ethers.getSigners();
+      addresses = [creator.address, alice.address, bob.address, chad.address, dad.address]
+      const Token = await ethers.getContractFactory('TestToken');
+      const Distributor = await ethers.getContractFactory('DevFeeForwarder');
+
+      token = await Token.deploy(name, simbol, decimal);
+      dstr = await Distributor.deploy(alice.address);
+    });
+
+    describe("Condition", function () {
+        it("contract should be deployed", async () => {
+            expect(await token.address).to.exist;
+            expect(await dstr.address).to.exist;
+            expect(await dstr.wallet()).to.equal(alice.address);
+        });
+    });
+
+    describe("constructor()", function(){
+        it("deploy should be reverted", async () => {
+            const Distributor = await ethers.getContractFactory('DevFeeForwarder');
+            await expect(Distributor.deploy(ZERO_ADDRESS)).to.revertedWith("zero address");
+        });
+    });
+
+    describe("distribute()", function () {
+        it("test_distribute_success", async () => {
+            await token._mint_for_testing(1000);
+            await token.approve(dstr.address, 1000);
+
+
+            //creator has 1000, and approve 1000 to the dstr
+            expect(await token.balanceOf(creator.address)).to.equal(1000);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(1000);
+            expect(await token.balanceOf(dstr.address)).to.equal(0);
+            expect(await token.balanceOf(alice.address)).to.equal(0);
+
+            await dstr.distribute(token.address);
+
+            expect(await token.balanceOf(creator.address)).to.equal(0);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(0);
+            expect(await token.balanceOf(dstr.address)).to.equal(0);
+            expect(await token.balanceOf(alice.address)).to.equal(1000);
+        });
+
+        it("test_distribute_success_all_token", async () => {
+            await token._mint_for_testing(1500);
+            await token.approve(dstr.address, 1000);
+            await token.transfer(dstr.address, 500);
+
+
+            //creator has 1000, and approve 1000 to the dstr
+            expect(await token.balanceOf(creator.address)).to.equal(1000);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(1000);
+            expect(await token.balanceOf(dstr.address)).to.equal(500);
+            expect(await token.balanceOf(alice.address)).to.equal(0);
+
+            await dstr.distribute(token.address);
+
+            expect(await token.balanceOf(creator.address)).to.equal(0);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(0);
+            expect(await token.balanceOf(dstr.address)).to.equal(0);
+            expect(await token.balanceOf(alice.address)).to.equal(1500);
+        });
+
+        it("test_distribute_success_no_claimable", async () => {
+            await token._mint_for_testing(1000);
+            await token.approve(dstr.address, 0);
+            await token.transfer(dstr.address, 500);
+
+
+            //creator has 1000, and approve 1000 to the dstr
+            expect(await token.balanceOf(creator.address)).to.equal(500);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(0);
+            expect(await token.balanceOf(dstr.address)).to.equal(500);
+            expect(await token.balanceOf(alice.address)).to.equal(0);
+
+            await dstr.distribute(token.address);
+
+            expect(await token.balanceOf(creator.address)).to.equal(500);
+            expect(await token.allowance(creator.address, dstr.address)).to.equal(0);
+            expect(await token.balanceOf(dstr.address)).to.equal(0);
+            expect(await token.balanceOf(alice.address)).to.equal(500);
+        });
+
+        it("test_distribute_fail", async () => {
+
+            await expect(dstr.distribute(ZERO_ADDRESS)).to.revertedWith("zero address");
+        });
+
+        
+    });
+});
