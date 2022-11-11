d3171c615932e547d5081f32de22e52daee75e52
maguro <maguro.devlife@gmail.com>
2022-11-11 09:49:52 +0900

    refactor: defined interface, inherited it

--- contracts/FeeDistribution/GovFeeDistributor.sol
+++ contracts/FeeDistribution/GovFeeDistributor.sol
@@ -11,6 +11,7 @@ import {VeCheckpointLogic} from "../libraries/VeCheckpointLogic.sol";
 
 import {VotingEscrow} from "../VotingEscrow.sol";
 
+import {IGovFeeDistributor} from "../interfaces/dao/IGovFeeDistributor.sol";
 import {ICDSTemplate} from "../interfaces/pool/ICDSTemplate.sol";
 import {IOwnership} from "../interfaces/pool/IOwnership.sol";
 
@@ -24,7 +25,7 @@ import {OnlyOwner, AddressZero, AmountZero, ContractUnavailable, InsufficientBal
  *         to veINSURE holders.
  */
 
-contract GovFeeDistributor is ReentrancyGuard {
+contract GovFeeDistributor is ReentrancyGuard, IGovFeeDistributor {
     using SafeERC20 for IERC20;
     using SafeCast for int256;
     using SafeCast for uint256;
@@ -130,18 +131,12 @@ contract GovFeeDistributor is ReentrancyGuard {
      * external functions
      */
 
-    /**
-     * @notice deposits all govanance fee this contract has,
-     *         then receives iToken(Reserve pool's LP token).
-     */
+    /// @inheritdoc IGovFeeDistributor
     function depositBalanceToReserve() external nonReentrant notKilled {
         _depositBalanceToReserve(IERC20(depositToken).balanceOf(address(this)));
     }
 
-    /**
-     * @notice overload function to specify the amount for LP token conversion.
-     * @param _amount the amount deposited into reserve pool
-     */
+    /// @inheritdoc IGovFeeDistributor
     function depositBalanceToReserve(uint256 _amount)
         external
         nonReentrant
@@ -150,11 +145,7 @@ contract GovFeeDistributor is ReentrancyGuard {
         _depositBalanceToReserve(_amount);
     }
 
-    /**
-     * @notice claim all eligible amount of iToken to msg sender.
-     *         this should be called by a veINSURE holder.
-     * @return claimed iToken amount
-     */
+    /// @inheritdoc IGovFeeDistributor
     function claim()
         external
         nonReentrant
@@ -165,12 +156,7 @@ contract GovFeeDistributor is ReentrancyGuard {
         return _claim(msg.sender);
     }
 
-    /**
-     * @notice claim all eligible amount of iToken on behalf of holder.
-     *         anyone can call this function.
-     * @param _to veINSURE holder address all claimed reward send
-     * @return claimed iToken amount
-     */
+    /// @inheritdoc IGovFeeDistributor
     function claim(address _to)
         external
         nonReentrant
@@ -182,14 +168,7 @@ contract GovFeeDistributor is ReentrancyGuard {
         return _claim(_to);
     }
 
-    /**
-     * @notice execute claim for multiple addresses, this used for multi user distribution,
-     *         or claim for same user who has large veINSURE hisrory.
-     * @param _receivers the addresses of veINSURE holders.
-     * @dev you can include same addresses as params for large veINSURE history.
-     * @dev addresses should be left aligned, otherwise claim will be cancelled in the middle of process.
-     * @return claim success
-     */
+    /// @inheritdoc IGovFeeDistributor
     function claimMany(address[20] calldata _receivers)
         external
         nonReentrant
@@ -211,18 +190,12 @@ contract GovFeeDistributor is ReentrancyGuard {
         return true;
     }
 
-    /**
-     * @notice checkpoints veINSURE total supply each week.
-     *         see VeCheckpointLogic.sol for more details.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function veSupplyCheckpoint() external {
         VeCheckpointLogic.checkpoint(votingEscrow, veCheckpointRecord);
     }
 
-    /**
-     * @notice checkpoints iToken(Reseve pool LP token) total supply each week.
-     *         see ITokenCheckpointLogic.sol for more details.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function iTokenCheckPoint() external {
         ITokenCheckpointLogic.checkpoint(
             iToken,
@@ -231,12 +204,7 @@ contract GovFeeDistributor is ReentrancyGuard {
         );
     }
 
-    /**
-     * @notice burn all iToken of msg.sender.
-     * @dev technically, token does not go to address(0) but goes to this contract.
-     *      so burning increases this contract balance and distributed to holders again.
-     * @return burn success
-     */
+    /// @inheritdoc IGovFeeDistributor
     function burn() external nonReentrant notKilled returns (bool) {
         uint256 _amount = IERC20(iToken).balanceOf(msg.sender);
 
@@ -260,10 +228,7 @@ contract GovFeeDistributor is ReentrancyGuard {
         return true;
     }
 
-    /**
-     * @notice deactivate the distributor contract. once killed,
-     *         the contract permanently unavailable.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function killMe(address _to) external nonReentrant onlyOwner {
         isKilled = true;
 
@@ -277,18 +242,12 @@ contract GovFeeDistributor is ReentrancyGuard {
         emit Killed(block.timestamp);
     }
 
-    /**
-     * @notice get last checkpointed iToken balance.
-     * @return last checkpointed iToken balance.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function lastITokenBalance() external view returns (uint256) {
         return iTokenCheckpointRecord.lastITokenBalance;
     }
 
-    /**
-     * @notice get last iToken checkpointed time.
-     * @return last iToken checkpointed time.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function lastITokenTime() external view returns (uint256) {
         return iTokenCheckpointRecord.lastITokenTime;
     }
@@ -296,22 +255,13 @@ contract GovFeeDistributor is ReentrancyGuard {
     /**
      * public functions
      */
-    /**
-     * @notice get total iToken distribution of a week.
-     * @param _weekCursor week boundary distribution activated
-     * @dev week cursor must be rounded for the start of a week.
-     * @return total iToken distribution of a week.
-     */
+
+    /// @inheritdoc IGovFeeDistributor
     function iTokenSupplyAt(uint256 _weekCursor) public view returns (uint256) {
         return iTokenCheckpointRecord.iTokenSupplyPerWeek[_weekCursor];
     }
 
-    /**
-     * @notice get total veINSURE supply of a week.
-     * @param _weekCursor week boundary iToken distribution activated
-     * @dev week cursor must be rounded for the start of a week.
-     * @return total veINSURE supply of a week.
-     */
+    /// @inheritdoc IGovFeeDistributor
     function veSupplyAt(uint256 _weekCursor) public view returns (uint256) {
         return veCheckpointRecord.veSupplyPerWeek[_weekCursor];
     }
---
+++ contracts/interfaces/dao/IGovFeeDistributor.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.10;
+
+interface IGovFeeDistributor {
+    /**
+     * @notice deposits all govanance fee this contract has,
+     *         then receives iToken(Reserve pool's LP token).
+     */
+    function depositBalanceToReserve() external;
+
+    /**
+     * @notice overload function to specify the amount for LP token conversion.
+     * @param _amount the amount deposited into reserve pool
+     */
+    function depositBalanceToReserve(uint256 _amount) external;
+
+    /**
+     * @notice claim all eligible amount of iToken to msg sender.
+     *         this should be called by a veINSURE holder.
+     * @return claimed iToken amount
+     */
+    function claim() external returns (uint256);
+
+    /**
+     * @notice claim all eligible amount of iToken on behalf of holder.
+     *         anyone can call this function.
+     * @param _to veINSURE holder address all claimed reward send
+     * @return claimed iToken amount
+     */
+    function claim(address _to) external returns (uint256);
+
+    /**
+     * @notice execute claim for multiple addresses, this used for multi user distribution,
+     *         or claim for same user who has large veINSURE hisrory.
+     * @param _receivers the addresses of veINSURE holders.
+     * @dev you can include same addresses as params for large veINSURE history.
+     * @dev addresses should be left aligned, otherwise claim will be cancelled in the middle of process.
+     * @return claim success
+     */
+    function claimMany(address[20] calldata _receivers) external returns (bool);
+
+    /**
+     * @notice checkpoints veINSURE total supply each week.
+     *         see VeCheckpointLogic.sol for more details.
+     */
+    function veSupplyCheckpoint() external;
+
+    /**
+     * @notice checkpoints iToken(Reseve pool LP token) total supply each week.
+     *         see ITokenCheckpointLogic.sol for more details.
+     */
+    function iTokenCheckPoint() external;
+
+    /**
+     * @notice burn all iToken of msg.sender.
+     * @dev technically, token does not go to address(0) but goes to this contract.
+     *      so burning increases this contract balance and distributed to holders again.
+     * @return burn success
+     */
+    function burn() external returns (bool);
+
+    /**
+     * @notice deactivate the distributor contract. once killed,
+     *         the contract permanently unavailable.
+     */
+    function killMe(address _to) external;
+
+    /**
+     * @notice get last checkpointed iToken balance.
+     * @return last checkpointed iToken balance.
+     */
+    function lastITokenBalance() external view returns (uint256);
+
+    /**
+     * @notice get last iToken checkpointed time.
+     * @return last iToken checkpointed time.
+     */
+    function lastITokenTime() external view returns (uint256);
+
+    /**
+     * @notice get total iToken distribution of a week.
+     * @param _weekCursor week boundary distribution activated
+     * @dev week cursor must be rounded for the start of a week.
+     * @return total iToken distribution of a week.
+     */
+    function iTokenSupplyAt(uint256 _weekCursor)
+        external
+        view
+        returns (uint256);
+
+    /**
+     * @notice get total veINSURE supply of a week.
+     * @param _weekCursor week boundary iToken distribution activated
+     * @dev week cursor must be rounded for the start of a week.
+     * @return total veINSURE supply of a week.
+     */
+    function veSupplyAt(uint256 _weekCursor) external view returns (uint256);
+}
